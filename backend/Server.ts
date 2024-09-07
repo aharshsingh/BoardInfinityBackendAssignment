ebed089fdc66df7bb1e96afef228f69049317901
aharshsingh <aharshsingh25@gmail.com>
2024-09-08 00:49:10 +0530

    serverSetup

---
+++ backend/Server.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const { APP_PORT, DB_URL } = require('./config');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
+app.listen(APP_PORT, () => {
+    console.log(`Listening on ${APP_PORT}`);
+});
